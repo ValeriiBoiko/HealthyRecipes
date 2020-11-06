306aa005e43f6470badf7bc7486b6286daaec443
ValeriiBoiko <valerii.boiko.1998@gmail.com>
2020-11-06 21:44:17 +0200

    split getRecipe service

--- service/index.js
+++ service/index.js
@@ -1,9 +1,7 @@
-const RECIPE_URL = 'https://api.spoonacular.com/recipes/';
-const SEARCH_URL = 'https://api.spoonacular.com/recipes/complexSearch';
 const key = '05c8053849544c2cbd4e93584ad9aafc';
 
 export function getRecipes(config) {
-  let url = SEARCH_URL + '?apiKey=' + key;
+  let url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${key}`;
   let query = '';
 
   for (param in config) {
@@ -29,14 +27,13 @@ export function getRecipes(config) {
 }
 
 export function getRecipe(id) {
-  let recipeUrl = RECIPE_URL + id + '/information?apiKey=' + key;
-  let instructionUrl = 'https://api.spoonacular.com/recipes/' + id + '/analyzedInstructions/?apiKey=' + key;
+  let recipeUrl = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${key}`;
 
   return new Promise((resolve, reject) => {
     fetch(recipeUrl)
       .then(resp => resp.json())
       .then(rawRecipe => {
-        const recipe = {
+        resolve({
           id: rawRecipe.id,
           title: rawRecipe.title,
           image: rawRecipe.image,
@@ -49,51 +46,50 @@ export function getRecipe(id) {
             id: item.id,
             title: item.original,
           })),
-        }
-
-        new Promise((res, rej) => {
-          fetch(instructionUrl)
-            .then(resp => resp.json())
-            .then(resp => {
-              const result = [];
-
-              resp.forEach((instruction) => {
-                instruction.steps.forEach((step) => {
-                  result.push(step.step)
-                })
-              })
-
-              resolve({
-                ...recipe,
-                instructions: result
-              });
-            })
-            .catch(err => rej(err));
         })
-          .catch(err => reject(err));
-      });
+      })
+      .catch(err => reject(err));
+  });
+}
 
-    // return new Promise((resolve, reject) => {
-    //   fetch(recipeUrl)
-    //     .then(resp => resp.json())
-    //     .then(rawRecipe => {
-    //       const recipe = {
-    //         id: rawRecipe.id,
-    //         title: rawRecipe.title,
-    //         image: rawRecipe.image,
-    //         servings: rawRecipe.servings,
-    //         readyInMinutes: rawRecipe.readyInMinutes,
-    //         sourceName: rawRecipe.sourceName,
-    //         summary: rawRecipe.summary,
-    //         aggregateLikes: rawRecipe.aggregateLikes,
-    //         ingredients: rawRecipe.extendedIngredients.map((item) => ({
-    //           id: item.id,
-    //           title: item.original,
-    //         })),
-    //       }
+export function getInstructions(recipeId) {
+  let instructionUrl = `https://api.spoonacular.com/recipes/${recipeId}/analyzedInstructions/?apiKey=${key}`;
 
-    //       resolve(recipe);
-    //     })
-    //     .catch(err => reject(err))
+  return new Promise((resolve, reject) => {
+    fetch(instructionUrl)
+      .then(resp => resp.json())
+      .then(resp => {
+        const result = [];
+
+        resp.forEach((instruction) => {
+          instruction.steps.forEach((step) => {
+            result.push(step.step)
+          })
+        })
+
+        resolve(result);
+      })
+      .catch(err => rej(err));
+  })
+}
+
+export function getSimilarRecipes(recipeId, number = 3) {
+  let url = `https://api.spoonacular.com/recipes/${recipeId}/similar?apiKey=${key}&number=${number}`;
+
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(resp => resp.json())
+      .then(recipes => {
+
+        const result = recipes.map(recipe => ({
+          id: recipe.id,
+          title: recipe.title,
+          readyInMinutes: recipe.readyInMinutes,
+          image: `https://spoonacular.com/recipeImages/${recipe.id}-312x231.${recipe.imageType}`
+        }))
+
+        resolve(result)
+      })
+      .catch(err => reject(err))
   })
 }
