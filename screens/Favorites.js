e8337f3f655cce234f5acdbe59e933749cee591d
ValeriiBoiko <valerii.boiko.1998@gmail.com>
2020-11-07 09:56:27 +0200

    update Favorites screen to display recipes

--- screens/Favorites.js
+++ screens/Favorites.js
@@ -1,16 +1,47 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { connect } from 'react-redux';
+import Recipe from '../components/Recipe';
+import { wp } from '../utils';
 
 function Favorites(props) {
+  const recipes = props.recipes.map((recipe) => (
+    <Recipe key={recipe.id} style={styles.recipe} {...recipe} onPress={() => {
+      props.navigation.navigate('Recipe', { recipeId: recipe.id })
+    }} />
+  ));
+
   return (
-    <View style={{
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    }}>
-      <Text>Favorites</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView
+        style={styles.scrollView}
+        contentContainerStyle={styles.containerStyle}>
+        {recipes}
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
-export default Favorites;
+const styles = StyleSheet.create({
+  scrollView: {
+    paddingHorizontal: wp(16)
+  },
+  containerStyle: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
+  },
+  recipe: {
+    flexGrow: 0,
+    flexShrink: 0,
+    flexBasis: '50%',
+    padding: wp(4),
+  }
+});
+
+const mapStateToProps = (state) => ({
+  recipes: state.favorites,
+});
+
+export default connect(mapStateToProps)(Favorites);
