9203c6501785820358c2f11081ac431d202ed628
ValeriiBoiko <valerii.boiko.1998@gmail.com>
2020-11-16 08:22:36 +0200

    show categories by cycle and update paddings

--- screens/RecipesCategories.js
+++ screens/RecipesCategories.js
@@ -1,57 +1,87 @@
 import { useTheme } from '@react-navigation/native';
 import React, { useMemo } from 'react';
 import { Image, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Font } from '../constants/Design';
 import { wp } from '../utils';
 
 function RecipesCategories(props) {
   const { colors } = useTheme();
+  const insets = useSafeAreaInsets();
   const styles = useMemo(() => getStyles(colors), [colors])
+  const categories = [
+    {
+      title: 'All Recipes',
+      image: require('../assets/images/all-recipes.jpg'),
+      diet: null,
+    },
+    {
+      title: 'Vegan',
+      image: require('../assets/images/vegan.jpg'),
+      diet: 'vegan',
+    },
+    {
+      title: 'Vegetarian',
+      image: require('../assets/images/vegetables.jpg'),
+      diet: 'vegetarian',
+    },
+    {
+      title: 'Gluten Free',
+      image: require('../assets/images/gluten-free.jpg'),
+      diet: 'gluten free',
+    },
+    {
+      title: 'Ketogenic',
+      image: require('../assets/images/ketogenic.jpg'),
+      diet: 'ketogenic',
+    },
+    {
+      title: 'Paleo',
+      image: require('../assets/images/paleo.jpg'),
+      diet: 'paleo',
+    },
+    {
+      title: 'Whole30',
+      image: require('../assets/images/whole30.jpg'),
+      diet: 'whole30',
+    },
+    {
+      title: 'Primal',
+      image: require('../assets/images/primal.jpg'),
+      diet: 'primal',
+    }
+  ];
+
+  const renderCategories = categories.map(cat => (
+    <Pressable key={cat.title} onPress={() => {
+      props.navigation.navigate('Recipes', {
+        diet: cat.diet,
+      })
+    }}>
+      <View style={styles.category}>
+        <Image source={cat.image} style={styles.categoryImage} />
+        <View style={[styles.categoryCaption, {
+          backgroundColor: colors.backgroundWithOpacity(0.85),
+        }]}>
+          <Text style={styles.primaryText}>Food book</Text>
+          <Text style={styles.categoryTitle}>{cat.title}</Text>
+        </View>
+      </View>
+    </Pressable>
+  ))
 
   return (
     <SafeAreaView>
-      <ScrollView style={{ paddingHorizontal: wp(20) }}>
+      <ScrollView contentContainerStyle={{
+        paddingHorizontal: wp(20),
+        paddingTop: insets.top > 0 ? 0 : wp(20),
+      }}>
         <Text style={styles.screenTitle}>Recipe categories</Text>
         <Text style={styles.primaryText}>
           Browse through our various recipes
         </Text>
 
-        <Pressable onPress={() => { props.navigation.navigate('Recipes') }}>
-          <View style={styles.category}>
-            <Image source={require('../assets/images/all-recipes.jpg')} style={styles.categoryImage} />
-            <View style={[styles.categoryCaption, {
-              backgroundColor: colors.backgroundWithOpacity(0.85),
-            }]}>
-              <Text style={styles.primaryText}>Food book</Text>
-              <Text style={styles.categoryTitle}>All Recipes</Text>
-            </View>
-          </View>
-        </Pressable>
-
-        <Pressable onPress={() => { props.navigation.navigate('Recipes') }}>
-          <View style={styles.category}>
-            <Image source={require('../assets/images/vegan.jpg')} style={styles.categoryImage} />
-            <View style={[styles.categoryCaption, {
-              backgroundColor: colors.backgroundWithOpacity(0.85),
-            }]}>
-              <Text style={styles.primaryText}>Food book</Text>
-              <Text style={styles.categoryTitle}>All Recipes</Text>
-            </View>
-          </View>
-        </Pressable>
-
-        <Pressable onPress={() => { props.navigation.navigate('Recipes') }}>
-          <View style={styles.category}>
-            <Image source={require('../assets/images/vegetables.jpg')} style={styles.categoryImage} />
-            <View style={[styles.categoryCaption, {
-              backgroundColor: colors.backgroundWithOpacity(0.85),
-            }]}>
-              <Text style={styles.primaryText}>Food book</Text>
-              <Text style={styles.categoryTitle}>All Recipes</Text>
-            </View>
-          </View>
-        </Pressable>
+        {renderCategories}
       </ScrollView>
     </SafeAreaView>
   )
@@ -62,6 +92,7 @@ const getStyles = (colors) => StyleSheet.create({
     fontFamily: Font.bold,
     fontSize: wp(24),
     lineHeight: wp(30),
+    color: colors.text,
   },
   primaryText: {
     fontFamily: Font.bold,
@@ -90,6 +121,7 @@ const getStyles = (colors) => StyleSheet.create({
     fontFamily: Font.bold,
     fontSize: wp(21),
     lineHeight: wp(28),
+    color: colors.text
   },
 });
 
