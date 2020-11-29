71d119fc21c63e6d73ffe266e4ccc62a1de88160
ValeriiBoiko <valerii.boiko.1998@gmail.com>
2020-11-29 14:25:12 +0200

    add tests for actions and apply eslint format

--- __tests__/Thunks.test.js
+++ __tests__/Thunks.test.js
@@ -1,31 +1,287 @@
-import Action from "../constants/Action";
-import { addRecipes } from "../middleware";
+import Action from '../constants/Action';
+import {
+  addRecipes,
+  addToCart,
+  addToFavorite,
+  removeFromCart,
+  removeFromFavorite,
+  removeRecipeFromCart,
+  setRecipe,
+  setRecipes,
+} from '../middleware';
 
 jest.mock('../service', () => ({
-  getRecipes: () => Promise.resolve([{ id: 1, title: 'foo' }])
-}))
+  getRecipes: () => Promise.resolve([{id: 1, title: 'foo'}]),
+  getRecipe: () => Promise.resolve({id: 1, title: 'foo'}),
+  getInstructions: () => Promise.resolve([]),
+}));
 
-describe('test thunk Actions', () => {
-  it('addRecipes action should dispatch SET_RECIPES action', async () => {
+describe('test actions', () => {
+  it.each([
+    [
+      'cart',
+      {
+        cart: [
+          {id: 0, title: 'bar'},
+          {id: 1, title: 'foo'},
+        ],
+      },
+    ],
+    [
+      'favorites',
+      {
+        cart: [
+          {
+            id: 0,
+            title: 'bar',
+          },
+        ],
+        favorites: [
+          {
+            id: 1,
+            title: 'foo',
+          },
+        ],
+      },
+    ],
+  ])(
+    'addRecipes action should dispatch SET_RECIPES action with correct payload',
+    async (type, payload) => {
+      const dispatch = jest.fn();
+      const getState = jest.fn().mockReturnValueOnce({
+        recipes: {
+          cart: [
+            {
+              id: 0,
+              title: 'bar',
+            },
+          ],
+        },
+      });
+
+      await addRecipes({number: 1}, type)(dispatch, getState);
+
+      expect(dispatch).toBeCalledWith({
+        type: Action.SET_RECIPES,
+        payload: payload,
+      });
+    },
+  );
+
+  it('setRecipes action should replace existing recipes', async () => {
     const dispatch = jest.fn();
     const getState = jest.fn().mockReturnValueOnce({
       recipes: {
-        cart: [{ id: 0, title: 'bar' }]
-      }
+        cart: [
+          {
+            id: 123,
+          },
+        ],
+      },
     });
 
-    await addRecipes({ number: 1 }, 'cart')(dispatch, getState);
-
-    expect(dispatch.mock.calls.length).toBe(1)
+    await setRecipes({number: 1}, 'cart')(dispatch, getState);
 
     expect(dispatch).toBeCalledWith({
       type: Action.SET_RECIPES,
       payload: {
         cart: [
-          { id: 0, title: 'bar' },
-          { id: 1, title: 'foo' }
-        ]
-      }
-    })
-  })
-})
+          {
+            id: 1,
+            title: 'foo',
+          },
+        ],
+      },
+    });
+  });
+
+  it('setRecipe action should replace existing recipe for specified recipes type', async () => {
+    const dispatch = jest.fn();
+    const getState = jest.fn().mockReturnValueOnce({recipe: {}});
+
+    await setRecipe(1, 'cart')(dispatch, getState);
+
+    expect(dispatch.mock.calls[0][0]).toEqual(
+      expect.objectContaining({
+        type: Action.SET_RECIPE,
+        payload: {
+          state: 'LOADING',
+        },
+      }),
+    );
+    expect(dispatch.mock.calls[1][0]).toEqual(
+      expect.objectContaining({
+        payload: {
+          cart: {
+            id: 1,
+            title: 'foo',
+            instructions: [],
+            state: 'READY',
+          },
+        },
+        type: Action.SET_RECIPE,
+      }),
+    );
+  });
+
+  it.each([
+    ['123', {}],
+    [
+      '321',
+      {
+        123: {
+          ingredients: [],
+        },
+      },
+    ],
+    [
+      undefined,
+      {
+        123: {
+          ingredients: [],
+        },
+      },
+    ],
+  ])(
+    'removeRecipeFromCart action should delete the recipe from cart by id',
+    async (id, payload) => {
+      const dispatch = jest.fn();
+      const getState = jest.fn().mockReturnValueOnce({
+        cart: {
+          123: {ingredients: []},
+        },
+      });
+
+      await removeRecipeFromCart(id)(dispatch, getState);
+
+      expect(dispatch).toBeCalledWith({
+        type: Action.UPDATE_CART,
+        payload: payload,
+      });
+    },
+  );
+
+  it.each([
+    [{id: 123}, {id: 0}, {123: {ingredients: [{id: 1}]}}],
+    [{id: 123}, {id: 1}, {123: {ingredients: [{id: 0}]}}],
+  ])(
+    'removeFromCart action should delete ingredient from cart',
+    async (recipe, ingredient, payload) => {
+      const dispatch = jest.fn();
+      const getState = jest.fn().mockReturnValueOnce({
+        cart: {
+          123: {ingredients: [{id: 0}, {id: 1}]},
+        },
+      });
+
+      await removeFromCart(recipe, ingredient)(dispatch, getState);
+
+      expect(dispatch).toBeCalledWith({
+        type: Action.UPDATE_CART,
+        payload: payload,
+      });
+    },
+  );
+
+  it('removeFromCart action should delete recipe if the last ingredient is deleted', async () => {
+    const dispatch = jest.fn();
+    const getState = jest.fn().mockReturnValueOnce({
+      cart: {
+        123: {ingredients: [{id: 0}]},
+      },
+    });
+
+    await removeFromCart({id: 123}, {id: 0})(dispatch, getState);
+
+    expect(dispatch).toBeCalledWith({
+      type: Action.UPDATE_CART,
+      payload: {},
+    });
+  });
+
+  it('addToCart action should add new recipe and ingredient if there is no recipe in the cart', async () => {
+    const recipe = {
+      id: 123,
+      image: 'image://path',
+      title: 'recipeTitle',
+    };
+    const dispatch = jest.fn();
+    const getState = jest.fn().mockReturnValueOnce({cart: {}});
+
+    await addToCart(recipe, {id: 0})(dispatch, getState);
+
+    expect(dispatch).toBeCalledWith({
+      type: Action.UPDATE_CART,
+      payload: {
+        123: {
+          ...recipe,
+          ingredients: [{id: 0}],
+        },
+      },
+    });
+  });
+
+  it('addToCart action should add ingredient to existing recipe in the cart', async () => {
+    const recipe = {
+      id: 123,
+      image: 'image://path',
+      title: 'recipeTitle',
+      ingredients: [{id: 0}],
+    };
+    const dispatch = jest.fn();
+    const getState = jest.fn().mockReturnValueOnce({
+      cart: {
+        123: recipe,
+      },
+    });
+
+    await addToCart(recipe, {id: 1})(dispatch, getState);
+
+    expect(dispatch).toBeCalledWith({
+      type: Action.UPDATE_CART,
+      payload: {
+        123: {
+          ...recipe,
+          ingredients: [{id: 0}, {id: 1}],
+        },
+      },
+    });
+  });
+
+  it.each([
+    [123, []],
+    [321, [{id: 123}]],
+    [321, [{id: 123}], [{id: 123}, {id: 321}]],
+  ])(
+    'removeFromFavorite action should delete recipe from favorites',
+    async (id, payload, init = [{id: 123}]) => {
+      const dispatch = jest.fn();
+      const getState = jest.fn().mockReturnValueOnce({favorites: init});
+
+      await removeFromFavorite(id)(dispatch, getState);
+
+      expect(dispatch).toBeCalledWith({
+        type: Action.SET_FAVORITES,
+        payload: payload,
+      });
+    },
+  );
+
+  it.each([
+    [{id: 123}, [{id: 123}]],
+    [{id: 321}, [{id: 123}, {id: 321}], [{id: 123}]],
+  ])(
+    'addToFavorite action should add recipe to favorites',
+    async (recipe, payload, init = []) => {
+      const dispatch = jest.fn();
+      const getState = jest.fn().mockReturnValueOnce({favorites: init});
+
+      await addToFavorite(recipe)(dispatch, getState);
+
+      expect(dispatch).toBeCalledWith({
+        type: Action.SET_FAVORITES,
+        payload: payload,
+      });
+    },
+  );
+});
--- middleware/index.js
+++ middleware/index.js
@@ -1,30 +1,32 @@
-import { batch } from "react-redux";
-import Action from "../constants/Action";
-import { getRecipes, getRecipe, getInstructions } from "../service";
+import Action from '../constants/Action';
+import {getRecipes, getRecipe, getInstructions} from '../service';
 
 export function addRecipes(config, recipesType) {
   return (dispatch, getState) => {
-    const { recipes } = getState();
+    const {recipes} = getState();
 
     if (config.number && config.number > 0) {
       getRecipes(config)
         .then((result) => {
+          result = recipes[recipesType]
+            ? recipes[recipesType].concat(result)
+            : result;
           dispatch({
             type: Action.SET_RECIPES,
             payload: {
               ...recipes,
-              [recipesType]: recipes[recipesType].concat(result)
-            }
-          })
+              [recipesType]: result,
+            },
+          });
         })
-        .catch((err) => console.log(err))
+        .catch((err) => console.log(err));
     }
-  }
+  };
 }
 
 export function setRecipes(config, recipesType) {
   return (dispatch, getState) => {
-    const { recipes } = getState();
+    const {recipes} = getState();
 
     if (config.number && config.number > 0) {
       getRecipes(config)
@@ -33,23 +35,25 @@ export function setRecipes(config, recipesType) {
             type: Action.SET_RECIPES,
             payload: {
               ...recipes,
-              [recipesType]: result
-            }
-          })
+              [recipesType]: result,
+            },
+          });
         })
-        .catch((err) => console.log(err))
+        .catch((err) => console.log(err));
     }
-  }
+  };
 }
 
 export function setRecipe(id = -1, type) {
-  if (!id && id !== 0) return null;
+  if (!id && id !== 0) {
+    return null;
+  }
 
   return (dispatch) => {
     dispatch({
       type: Action.SET_RECIPE,
-      payload: { state: 'LOADING' }
-    })
+      payload: {state: 'LOADING'},
+    });
 
     const recipe = getRecipe(id);
     const instructions = getInstructions(id);
@@ -63,12 +67,12 @@ export function setRecipe(id = -1, type) {
               ...values[0],
               instructions: values[1],
               state: 'READY',
-            }
-          }
-        })
+            },
+          },
+        });
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 }
 
 export function addToFavorite(recipe) {
@@ -78,55 +82,57 @@ export function addToFavorite(recipe) {
     dispatch({
       type: Action.SET_FAVORITES,
       payload: favorites.concat([recipe]),
-    })
-  }
+    });
+  };
 }
 
 export function removeFromFavorite(recipeId) {
   return (dispatch, getState) => {
-    const favorites = getState().favorites.filter(favorite => (
-      favorite.id !== recipeId
-    ));
+    const favorites = getState().favorites.filter(
+      (favorite) => favorite.id !== recipeId,
+    );
 
     dispatch({
       type: Action.SET_FAVORITES,
       payload: favorites,
-    })
-  }
+    });
+  };
 }
 
 export function addToCart(recipe, ingredient) {
   return (dispatch, getState) => {
-    const { cart } = getState();
-    const updatedCart = { ...cart };
+    const {cart} = getState();
+    const updatedCart = {...cart};
 
     if (!updatedCart[recipe.id]) {
       updatedCart[recipe.id] = {
         id: recipe.id,
         image: recipe.image,
         title: recipe.title,
-        ingredients: [ingredient]
-      }
+        ingredients: [ingredient],
+      };
     } else {
-      updatedCart[recipe.id].ingredients = updatedCart[recipe.id].ingredients.concat([ingredient]);
+      updatedCart[recipe.id].ingredients = updatedCart[
+        recipe.id
+      ].ingredients.concat([ingredient]);
     }
 
     dispatch({
       type: Action.UPDATE_CART,
-      payload: updatedCart
-    })
-  }
+      payload: updatedCart,
+    });
+  };
 }
 
 export function removeFromCart(recipe, ingredient) {
   return (dispatch, getState) => {
-    const { cart } = getState();
-    const updatedCart = { ...cart };
+    const {cart} = getState();
+    const updatedCart = {...cart};
     const cartRecipe = updatedCart[recipe.id];
 
-    cartRecipe.ingredients = cartRecipe.ingredients.filter((item) => (
-      item.id !== ingredient.id
-    ));
+    cartRecipe.ingredients = cartRecipe.ingredients.filter(
+      (item) => item.id !== ingredient.id,
+    );
 
     if (!cartRecipe.ingredients.length) {
       delete updatedCart[recipe.id];
@@ -134,23 +140,23 @@ export function removeFromCart(recipe, ingredient) {
 
     dispatch({
       type: Action.UPDATE_CART,
-      payload: updatedCart
-    })
-  }
+      payload: updatedCart,
+    });
+  };
 }
 
 export function removeRecipeFromCart(recipeId) {
   return (dispatch, getState) => {
-    const { cart } = getState();
-    const updatedCart = { ...cart };
+    const {cart} = getState();
+    const updatedCart = {...cart};
 
     if (updatedCart[recipeId]) {
-      delete updatedCart[recipeId]
+      delete updatedCart[recipeId];
     }
 
     dispatch({
       type: Action.UPDATE_CART,
-      payload: updatedCart
-    })
-  }
+      payload: updatedCart,
+    });
+  };
 }
