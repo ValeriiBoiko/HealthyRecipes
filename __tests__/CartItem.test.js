7f64ddbf0938e888ba2b05528a6c1fd1b507f457
ValeriiBoiko <valerii.boiko.1998@gmail.com>
2020-11-30 07:42:03 +0200

    apply es lint format to tests

--- __tests__/CartItem.test.js
+++ __tests__/CartItem.test.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
+import {render} from '@testing-library/react-native';
 import CartItem from '../components/CartItem';
 
 const defaultProps = {
   id: 0,
   title: 'testItemTitle',
   image: 'test://path.png',
-  ingredients: [{ title: 'testIngredient1' }, { title: 'testIngredient2' }]
-}
+  ingredients: [{title: 'testIngredient1'}, {title: 'testIngredient2'}],
+};
 
 describe('test CartItem component render', () => {
   it('cart item should be rendered with passed props', () => {
-    const { queryByTestId, queryByText, queryAllByText } = render(<CartItem  {...defaultProps} />);
+    const {queryByTestId, queryByText, queryAllByText} = render(
+      <CartItem {...defaultProps} />,
+    );
 
     expect(queryByText(/testItemTitle/)).toBeTruthy();
     expect(queryAllByText(/testIngredient/).length).toBe(2);
-    expect(queryByTestId('cartItemImage').props.source.uri).toBe('test://path.png');
+    expect(queryByTestId('cartItemImage').props.source.uri).toBe(
+      'test://path.png',
+    );
   });
-})
+});
--- __tests__/IngredientList.test.js
+++ __tests__/IngredientList.test.js
@@ -1,62 +1,68 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react-native';
+import {fireEvent, render} from '@testing-library/react-native';
 import IngredientList from '../components/IngredientList';
 
 const defaultProps = {
-  recipe: { id: 0 },
+  recipe: {id: 0},
   cart: {},
-  ingredients: []
-}
+  ingredients: [],
+};
 
 describe('test IngredientList component render', () => {
   it.each([
     [[], 0],
-    [[{ id: 0, title: 'testIngredient' }], 1],
-    [[{ id: 0, title: 'testIngredient' }, { id: 1, title: 'testIngredient' }], 2],
-  ])('List should render passed list of ingredients', (ingredient, expected) => {
-    const { queryAllByText } = render(
-      <IngredientList {...defaultProps} ingredients={ingredient} />
-    );
+    [[{id: 0, title: 'testIngredient'}], 1],
+    [
+      [
+        {id: 0, title: 'testIngredient'},
+        {id: 1, title: 'testIngredient'},
+      ],
+      2,
+    ],
+  ])(
+    'List should render passed list of ingredients',
+    (ingredient, expected) => {
+      const {queryAllByText} = render(
+        <IngredientList {...defaultProps} ingredients={ingredient} />,
+      );
 
-    expect(queryAllByText('testIngredient').length).toBe(expected)
-  })
+      expect(queryAllByText('testIngredient').length).toBe(expected);
+    },
+  );
 
   it('item should be added to cart if it is not there already', () => {
     const addToCart = jest.fn();
 
-    const { queryByTestId } = render(
-      <IngredientList {...defaultProps} ingredients={[
-        { id: 0, title: 'testIngredient' }
-      ]}
+    const {queryByTestId} = render(
+      <IngredientList
+        {...defaultProps}
+        ingredients={[{id: 0, title: 'testIngredient'}]}
         addToCart={addToCart}
-      />
+      />,
     );
 
     fireEvent.press(queryByTestId('addIngredient'));
-    expect(addToCart.mock.calls.length).toBe(1)
-  })
+    expect(addToCart.mock.calls.length).toBe(1);
+  });
 
   it('item should be remove from cart if it is there already', () => {
     const cart = {
-      "0": {
-        ingredients: [
-          { id: 0, title: 'testIngredient' }
-        ]
-      }
-    }
+      0: {
+        ingredients: [{id: 0, title: 'testIngredient'}],
+      },
+    };
     const removeFromCart = jest.fn();
 
-    const { queryByTestId } = render(
-      <IngredientList {...defaultProps}
+    const {queryByTestId} = render(
+      <IngredientList
+        {...defaultProps}
         cart={cart}
-        ingredients={[
-          { id: 0, title: 'testIngredient' }
-        ]}
+        ingredients={[{id: 0, title: 'testIngredient'}]}
         removeFromCart={removeFromCart}
-      />
+      />,
     );
 
     fireEvent.press(queryByTestId('removeIngredient'));
-    expect(removeFromCart.mock.calls.length).toBe(1)
-  })
-})
+    expect(removeFromCart.mock.calls.length).toBe(1);
+  });
+});
